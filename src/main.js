22777777458fc79e14ef164160baa70c52104938
TxTony <t.tipveau@berd.re>
2018-06-11 15:40:52 +0400

    Move vuex config

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import 'vuetify/dist/vuetify.css'
-import StoreIndex from './store/index.js'
 import App from './App'
 import router from './router'
 import services from './services.js'
 import plugins from './plugins.js'
 
 Vue.config.productionTip = false
-Vue.use(Vuex)
-var store = new Vuex.Store(StoreIndex)
+
 /* eslint-disable no-undef */
 var Root = services.addServices(Vue)
 Root = plugins.addPlugins(Root);
-const i18n = plugins.getI18nInstance();
+var store = plugins.getVuexInstance()
+const i18n = plugins.getI18nInstance()
 /* eslint-disable no-new */
 new Root({
     el: '#app',
--- src/plugins.js
+++ src/plugins.js
@@ -1,12 +1,14 @@
-
+import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import VueI18n from 'vue-i18n'
 import messages from './translations/appTranslation.js'
+import StoreIndex from './store/index.js'
 
 export default {
 
     addPlugins: function (Vue) {
         // define your plugins here
+        Vue.use(Vuex)
         Vue.use(Vuetify)
         Vue.use(VueI18n)
         // Create VueI18n instance with options
@@ -17,5 +19,8 @@ export default {
             locale: 'en', // set locale
             messages // set locale messages
         })
+    },
+    getVuexInstance: function(){
+        return new Vuex.Store(StoreIndex)
     }
 }
